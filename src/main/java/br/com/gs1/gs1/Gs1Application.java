19fb6d3a9e9a9437698828833e248e86f169ee26
fFukurou <supevi1@gmail.com>
2025-05-26 11:30:56 -0300

    initial files

---
+++ src/main/java/br/com/gs1/gs1/Gs1Application.java
@@ -0,0 +1,13 @@
+package br.com.gs1.gs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gs1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gs1/gs1/Gs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gs1.gs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
